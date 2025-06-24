c79ff38c53bd6c916bedea07828c08dd9348b271
if.dev_ <iara.freitas.job@gmail.com>
2025-06-24 16:05:39 -0300

    ok

--- public/index.php
+++ public/index.php
@@ -11,7 +11,6 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 
-
     <link rel="stylesheet" href="assets/css/slick.css">
     <link rel="stylesheet" href="assets/css/slick-theme.css">
     <link rel="stylesheet" href="assets/css/lity.min.css">
